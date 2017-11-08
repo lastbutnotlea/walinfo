9e94e7495d9ac038701b5042f13a18e88255756e
Lea R <n/a>
2017-11-08 19:08:18 +0100

    implemented postgres connection; added two sql files

--- wahlparser/src/main/java/PostgresConnection.java
+++ wahlparser/src/main/java/PostgresConnection.java
@@ -1,5 +1,71 @@
 /**
  * Created by Lea on 08.11.17.
  */
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PostgresConnection {
+
+    public static void main(String[] argv) throws SQLException {
+
+        Connection dbConnection = connectToDatabase();
+        if(dbConnection != null) {
+            fillTables(dbConnection);
+        }
+    }
+
+    public static void fillTables(Connection dbConnection) throws SQLException {
+        Statement statement = dbConnection.createStatement();
+        String fill1 = "insert into professoren values" +
+                "       (2125, 'Sokrates', 'C4', 226)";
+        statement.executeUpdate(fill1);
+    }
+
+    public static Connection connectToDatabase() {
+
+        System.out.println("-------- PostgreSQL "
+                + "JDBC Connection Testing ------------");
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Where is your PostgreSQL JDBC Driver? "
+                    + "Include in your library path!");
+            e.printStackTrace();
+            return null;
+
+        }
+
+        System.out.println("PostgreSQL JDBC Driver Registered!");
+
+        Connection connection = null;
+
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://127.0.0.1:5433/walinfo", "postgres",
+                    "mysecretpassword");
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return null;
+
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+            return connection;
+        } else {
+            System.out.println("Failed to make connection!");
+            return null;
+        }
+    }
+
 }
---
+++ wahlparser/src/res/sql-statements/create-tables.sql
@@ -0,0 +1,67 @@
+CREATE TABLE Bundeslaender (
+  kuerzel VARCHAR(2) PRIMARY KEY,
+  name VARCHAR(63)
+);
+
+CREATE TABLE Parteien (
+  id SMALLINT PRIMARY KEY,
+  kuerzel VARCHAR(63),
+  name VARCHAR(63),
+  farbe VARCHAR(31),
+  wahljahr SMALLINT
+);
+
+CREATE TABLE Wahlkreise (
+  id SMALLINT PRIMARY KEY,
+  name VARCHAR(63),
+  anzahl_wahlberechtigte INT,
+  bundesland VARCHAR(2) REFERENCES Bundeslaender(kuerzel),
+  wahljahr SMALLINT
+);
+
+CREATE TABLE Kandidaten (
+  id SMALLINT PRIMARY KEY,
+  name VARCHAR(63),
+  vorname VARCHAR(63),
+  titel VARCHAR(63),
+  namenszusatz VARCHAR(63),
+  geburtsjahr SMALLINT,
+  beruf VARCHAR(63),
+  geschlecht VARCHAR(1),
+  partei_id SMALLINT REFERENCES Parteien(id),
+  wahlkreis_id SMALLINT REFERENCES Wahlkreise(id),
+  wahljahr SMALLINT
+);
+
+CREATE TABLE Listenplaetze (
+  kandidaten_id SMALLINT REFERENCES Kandidaten(id) PRIMARY KEY,
+  bundesland VARCHAR(2) REFERENCES Bundeslaender(kuerzel),
+  listenplatz SMALLINT
+);
+
+CREATE TABLE Wahlzettel (
+  id INT PRIMARY KEY,
+  partei_id SMALLINT REFERENCES Parteien(id),
+  kandidaten_id SMALLINT REFERENCES Kandidaten(id),
+  gueltig VARCHAR(1)
+);
+
+CREATE TABLE Erststimmenergebnisse (
+  kandidaten_id SMALLINT REFERENCES Kandidaten(id),
+  wahlkreis_id SMALLINT REFERENCES Wahlkreise(id),
+  anzahl INT,
+  PRIMARY KEY (wahlkreis_id, kandidaten_id)
+);
+
+CREATE TABLE Zweitstimmenergebnisse (
+  partei_id SMALLINT REFERENCES Parteien(id),
+  wahlkreis_id SMALLINT REFERENCES Wahlkreise(id),
+  anzahl INT,
+  PRIMARY KEY (wahlkreis_id, partei_id)
+);
+
+CREATE TABLE Wahltoken (
+  token VARCHAR(127) PRIMARY KEY,
+  benutzt VARCHAR(1),
+  wahlkreis_id SMALLINT REFERENCES Wahlkreise(id)
+);
