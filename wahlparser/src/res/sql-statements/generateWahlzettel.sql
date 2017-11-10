15de7c7cf0fe795f5aa10fd16934f16b233dff49
Lea R <n/a>
2017-11-10 14:44:41 +0100

    fixed wahlzettel generator, added foreign keys

--- wahlparser/src/res/sql-statements/generateWahlzettel.sql
+++ wahlparser/src/res/sql-statements/generateWahlzettel.sql
@@ -1,15 +1,30 @@
-with leereTabelle(nummer) AS (
+with placeholder1(nummer) AS (
   SELECT * from generate_series(1, (select max(anzahl) from erststimmenergebnisse))
 )
   INSERT INTO erststimmen(kandidaten_id, wahlkreis_id, gueltig)
     SELECT e.kandidaten_id , e.wahlkreis_id, 'J'
-    FROM erststimmenergebnisse e, leereTabelle u
-    WHERE u.nummer <= e.anzahl
-  ;
+    FROM erststimmenergebnisse e, placeholder1 p1
+    WHERE p1.nummer <= e.anzahl
+;
 
-with leereTabelle(nummer) AS (
+with placeholder2(nummer) AS (
     SELECT * from generate_series(1, (select max(anzahl) from zweitstimmenergebnisse))
 )
-SELECT e.wahlkreis_id, e.partei_id
-  FROM zweitstimmenergebnisse e, leereTabelle u
-  WHERE u.nummer <= e.anzahl;
+  INSERT INTO zweitstimmen(partei_id, wahlkreis_id, gueltig)
+    SELECT e.partei_id, e.wahlkreis_id, 'J'
+    FROM zweitstimmenergebnisse e, placeholder2 p2
+    WHERE p2.nummer <= e.anzahl
+;
+
+-- afterwards
+ALTER TABLE erststimmen
+    ADD FOREIGN KEY (kandidaten_id) REFERENCES kandidaten(id);
+
+ALTER TABLE erststimmen
+    ADD FOREIGN KEY (wahlkreis_id) REFERENCES wahlkreise(nummer);
+
+ALTER TABLE zweitstimmen
+  ADD FOREIGN KEY (partei_id) REFERENCES parteien(id);
+
+ALTER TABLE zweitstimmen
+  ADD FOREIGN KEY (wahlkreis_id) REFERENCES wahlkreise(nummer);
