f7f5a911cf3af55a4f862b39b6e7072eed7d295a
Markus Sosnowski <m.sosnowski@soscomp.de>
2017-11-10 13:21:54 +0100

    Version 1 Stimmgenerator

--- wahlparser/src/res/sql-statements/generateWahlzettel.sql
+++ wahlparser/src/res/sql-statements/generateWahlzettel.sql
@@ -1,7 +1,14 @@
-with RECURSIVE unendlichkeit(nummer) as (
-  select 1
-  union ALL
-  select nummer + 1 from unendlichkeit
+with unendlichkeit(nummer) AS (
+  SELECT * from generate_series(1, (select max(anzahl) from erststimmenergebnisse))
 )
-select * from erststimmenergebnisse e, unendlichkeit u
-  where u.nummer <= e.anzahl;
+    SELECT e.wahlkreis_id, e.kandidaten_id
+    FROM erststimmenergebnisse e, unendlichkeit u
+    WHERE u.nummer <= e.anzahl
+  ;
+
+with unendlichkeit(nummer) AS (
+    SELECT * from generate_series(1, (select max(anzahl) from zweitstimmenergebnisse))
+)
+SELECT e.wahlkreis_id, e.partei_id
+  FROM zweitstimmenergebnisse e, unendlichkeit u
+  WHERE u.nummer <= e.anzahl;
