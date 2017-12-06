67b5c18d5334ae0dfdb359827bedb36ad54d4be1
Lea R <n/a>
2017-12-06 11:14:40 +0100

    splitted query 3-3 into two separate queries (erststimmen and zweitstimmen)

---
+++ wahlparser/src/res/sql-statements/q3-3-wahlkreis-erststimmen-pro-partei.sql
@@ -0,0 +1,30 @@
+WITH stimmen_gesamt AS (
+    SELECT
+      sum(erst.anzahl) AS gesamtstimmen,
+      erst.wahlkreis_id,
+      k.wahljahr
+    FROM erststimmenergebnisse erst, kandidaten k
+    WHERE
+          k.id = erst.kandidaten_id
+          AND erst.kandidaten_id IS NOT NULL
+    GROUP BY erst.wahlkreis_id, k.wahljahr
+)
+
+SELECT
+  p.kuerzel,
+  p.name,
+  p.farbe,
+  erst.anzahl AS anzahl_absolut,
+  CAST(erst.anzahl AS NUMERIC) /
+  (CAST((
+          SELECT gesamtstimmen
+          FROM stimmen_gesamt sg
+          WHERE sg.wahljahr = p.wahljahr
+                AND sg.wahlkreis_id = erst.wahlkreis_id
+        ) AS NUMERIC))       AS anzahl_relativ
+FROM erststimmenergebnisse erst, kandidaten k, parteien p
+WHERE erst.wahlkreis_id = k.wahlkreis_id
+      AND erst.kandidaten_id = k.id
+      AND k.partei_id = p.id
+      AND erst.kandidaten_id IS NOT NULL
+ORDER BY k.wahljahr, k.wahlkreis_id, p.name;
---
+++ wahlparser/src/res/sql-statements/q3-3-wahlkreis-zweitstimmen-pro-partei.sql
@@ -0,0 +1,27 @@
+WITH stimmen_gesamt AS (
+    SELECT
+      sum(zweit.anzahl) as zweitstimmen,
+      zweit.wahlkreis_id,
+      p.wahljahr
+    FROM zweitstimmenergebnisse zweit, parteien p
+    WHERE zweit.partei_id = p.id
+          AND zweit.partei_id IS NOT NULL
+    GROUP BY zweit.wahlkreis_id, p.wahljahr
+)
+
+SELECT
+  p.kuerzel,
+  p.name,
+  p.farbe,
+  zweit.anzahl AS anzahl_absolut,
+  CAST(zweit.anzahl AS NUMERIC) /
+  (CAST((
+          SELECT zweitstimmen
+          FROM stimmen_gesamt sg
+          WHERE sg.wahljahr = p.wahljahr
+                AND sg.wahlkreis_id = zweit.wahlkreis_id
+        ) AS NUMERIC))       AS anzahl_relativ
+FROM zweitstimmenergebnisse zweit, parteien p
+WHERE zweit.partei_id = p.id
+      AND zweit.partei_id IS NOT NULL
+ORDER BY p.wahljahr, zweit.wahlkreis_id, p.name;
